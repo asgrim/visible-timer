ed5650c6d0c45edee44995f29a6ff625c41a2d40
James Titcumb <james@asgrim.com>
2019-05-02 16:58:07 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Basic Thing For Timing Stuff
+
+ - Pre-timer for 3 seconds
+ - Time for number of seconds entered
+ - High visibility colours instead of timer sounds
---
+++ script.js
@@ -0,0 +1,45 @@
+var body = document.getElementsByTagName('body')[0];
+var timingWin = document.getElementById('timing');
+var setupWin = document.getElementById('setup');
+var noteText = document.getElementById('note');
+var secondsInput = document.getElementById('seconds');
+
+var preTimerRemaining;
+var preTimer;
+
+var actualTimeRemaining;
+var timer;
+
+function startTimer(preTime, actualTime) {
+    setupWin.setAttribute('class', 'hidden');
+    timingWin.setAttribute('class', '');
+    body.setAttribute('class', 'stopped');
+    preTimerRemaining = preTime;
+    actualTimeRemaining = actualTime;
+    noteText.innerText = preTimerRemaining;
+    preTimer = setInterval(function () {
+        preTimerRemaining--;
+        noteText.innerText = preTimerRemaining;
+
+        if (preTimerRemaining === 0) {
+            clearInterval(preTimer);
+            noteText.innerText = actualTimeRemaining;
+            body.setAttribute('class', 'started');
+            timer = setInterval(function () {
+                actualTimeRemaining--;
+                noteText.innerText = actualTimeRemaining;
+
+                if (actualTimeRemaining === 0) {
+                    clearInterval(timer);
+                    setupWin.setAttribute('class', '');
+                    timingWin.setAttribute('class', 'hidden');
+                    body.setAttribute('class', 'stopped');
+                }
+            }, 1000);
+        }
+    }, 1000);
+}
+
+document.getElementById('go').onclick = function () {
+    startTimer(3, parseInt(secondsInput.value));
+};
